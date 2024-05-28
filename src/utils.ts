39353c022e5352a05737153aa5269305e09854f9
jonathan tucker <github@jrtucker.com>
2024-05-28 15:28:19 -0700

    fixed safari bug

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { CSSProperties } from "react";
 export const getInteractionPosition = (
   e: MouseEvent | TouchEvent
 ): [clientX: number, clientY: number] => {
-  const { clientX, clientY } = e instanceof TouchEvent ? e.touches[0] : e;
+  const { clientX, clientY } = "touches" in e ? e.touches[0] : e;
 
   return [clientX, clientY];
 };
