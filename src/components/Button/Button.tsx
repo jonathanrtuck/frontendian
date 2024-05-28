3b7b3b4eacb24bc29dd6d2c0b8365e11603c5a2a
jonathan tucker <github@jrtucker.com>
2024-05-28 15:44:36 -0700

    created Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
+
+import styles from "./Button.module.css";
+
+export const Button: FunctionComponent<
+  ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ children, className, type = "button", ...props }) => {
+  //
+
+  return (
+    <button className={clsx(className, styles.root)} type={type} {...props}>
+      {children}
+    </button>
+  );
+};
