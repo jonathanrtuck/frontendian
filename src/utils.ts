a54682074b182d1d09735f08bf0b6e5abfcd3565
jonathan tucker <github@jrtucker.com>
2024-08-04 20:29:35 -0700

    cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,5 @@
 import { ComponentPropsWithoutRef } from "react";
 
-/* @todo
-export const getChildMenuitems = (element: HTMLElement | null): HTMLElement[] =>
-  Array.from<HTMLElement>(
-    element?.querySelectorAll(
-      ':scope > [role^="menuitem"], :scope > [role="menu"] > [role^="menuitem"]'
-    ) ?? []
-  );
-
-export const getSiblingMenuitems = (
-  element: HTMLElement | null
-): HTMLElement[] =>
-  element
-    ? getChildMenuitems(
-        element.closest('[role="menubar"], [role="menu"]')
-      ).filter((menuitem) => menuitem !== element)
-    : [];
-*/
-
 export const removeProps = <T extends ComponentPropsWithoutRef<any>>(
   props: T,
   propNames: string[]
