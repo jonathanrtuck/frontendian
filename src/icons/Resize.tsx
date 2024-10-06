e982ce088fe1f25d1b5f481d7767f315c0b01453
jonathan tucker <github@jrtucker.com>
2024-10-05 20:04:25 -0700

    cleanup

--- src/icons/Resize.tsx
+++ src/icons/Resize.tsx
@@ -33,48 +33,48 @@ export const Resize: IconComponent = forwardRef(
             version="1.1"
             viewBox="0 0 64 64"
             xmlns="http://www.w3.org/2000/svg">
-            <g strokeWidth="3">
+            <g strokeWidth="4">
               <line
                 stroke="var(--highlight-color)"
-                x1="11"
-                x2="43"
-                y1="43"
-                y2="11"
+                x1="12"
+                x2="38"
+                y1="38"
+                y2="12"
               />
               <line
                 stroke="var(--lowlight-color)"
-                x1="14"
+                x1="16"
+                x2="42"
+                y1="42"
+                y2="16"
+              />
+              <line
+                stroke="var(--highlight-color)"
+                x1="20"
                 x2="46"
                 y1="46"
-                y2="14"
-              />
-              <line
-                stroke="var(--highlight-color)"
-                x1="17"
-                x2="49"
-                y1="49"
-                y2="17"
-              />
-              <line
-                stroke="var(--lowlight-color)"
-                x1="20"
-                x2="52"
-                y1="52"
                 y2="20"
               />
               <line
+                stroke="var(--lowlight-color)"
+                x1="24"
+                x2="50"
+                y1="50"
+                y2="24"
+              />
+              <line
                 stroke="var(--highlight-color)"
-                x1="23"
-                x2="55"
-                y1="55"
-                y2="23"
+                x1="28"
+                x2="54"
+                y1="54"
+                y2="28"
               />
               <line
                 stroke="var(--lowlight-color)"
-                x1="26"
+                x1="32"
                 x2="58"
                 y1="58"
-                y2="26"
+                y2="32"
               />
             </g>
           </svg>
